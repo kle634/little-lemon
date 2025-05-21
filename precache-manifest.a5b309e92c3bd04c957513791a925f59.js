353b5c697573c9c75dbb577a631e98d3b34ea4b1
Kelly <kelly.le615@gmail.com>
2025-05-21 12:21:07 -0400

    Updates

---
+++ precache-manifest.a5b309e92c3bd04c957513791a925f59.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5357620379555be3417536e15f5cb1c6",
+    "url": "/little-lemon/index.html"
+  },
+  {
+    "revision": "debad1f36b894b9ad43c",
+    "url": "/little-lemon/static/css/main.cff5c3c0.chunk.css"
+  },
+  {
+    "revision": "e469a98b455dc29b2906",
+    "url": "/little-lemon/static/js/2.9ee6fc13.chunk.js"
+  },
+  {
+    "revision": "c61ae6acdcf184e9631d4f16a9434570",
+    "url": "/little-lemon/static/js/2.9ee6fc13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c8d81555402a91cc13",
+    "url": "/little-lemon/static/js/3.a802ad05.chunk.js"
+  },
+  {
+    "revision": "debad1f36b894b9ad43c",
+    "url": "/little-lemon/static/js/main.f87e3f46.chunk.js"
+  },
+  {
+    "revision": "7d0e790f0173c599fd91",
+    "url": "/little-lemon/static/js/runtime-main.75026e10.js"
+  },
+  {
+    "revision": "cf6501a3c796c2b5a20d709374c532e7",
+    "url": "/little-lemon/static/media/Baklava.cf6501a3.webp"
+  },
+  {
+    "revision": "f31de76df384e1cdcac582cc5d8c9cab",
+    "url": "/little-lemon/static/media/Beef-Gyro-1.f31de76d.jpg"
+  },
+  {
+    "revision": "daba418e38c0bcf5cdc41b4068e6fde6",
+    "url": "/little-lemon/static/media/Bruschetta_02.jpg.daba418e.webp"
+  },
+  {
+    "revision": "318b8dc489f26f38a9f1196d04550bbf",
+    "url": "/little-lemon/static/media/Logo.318b8dc4.svg"
+  },
+  {
+    "revision": "0597a0a51e452829a49ff837945244a5",
+    "url": "/little-lemon/static/media/Pistachio-Gelato-5.0597a0a5.jpg"
+  },
+  {
+    "revision": "b7931ef9c18e230eae98c7163f699387",
+    "url": "/little-lemon/static/media/bruchetta.b7931ef9.svg"
+  },
+  {
+    "revision": "8e8295b1fde35d49be570ca12524b61e",
+    "url": "/little-lemon/static/media/chicken_02.8e8295b1.webp"
+  },
+  {
+    "revision": "0110545b55eb0342afaa037376599430",
+    "url": "/little-lemon/static/media/greek salad.0110545b.jpg"
+  },
+  {
+    "revision": "9f76c60a8b7971f9fcfebca663dc7854",
+    "url": "/little-lemon/static/media/hummus_02.9f76c60a.jpg"
+  },
+  {
+    "revision": "35e576d2cfa4c9f70edfa62f3040e0a8",
+    "url": "/little-lemon/static/media/moussaka.35e576d2.jpg"
+  },
+  {
+    "revision": "6d770489e77689b70834efc5c02998b4",
+    "url": "/little-lemon/static/media/octopus.6d770489.jpg"
+  },
+  {
+    "revision": "ed3d2c3ea9b84740537c8d49477382f2",
+    "url": "/little-lemon/static/media/orange-cake.ed3d2c3e.jpg"
+  },
+  {
+    "revision": "52bd44af9fef8f4a1d13afc39cc00de7",
+    "url": "/little-lemon/static/media/restauranfood.52bd44af.jpg"
+  },
+  {
+    "revision": "bec110eea2abcb78f3018fbc117a05b1",
+    "url": "/little-lemon/static/media/restaurant chef B.bec110ee.jpg"
+  },
+  {
+    "revision": "fa4020257e0c41d1e3ba3fd9fed70467",
+    "url": "/little-lemon/static/media/restaurant.fa402025.jpg"
+  },
+  {
+    "revision": "c1a7862bfebd5578c79ae93141a2019b",
+    "url": "/little-lemon/static/media/tzatziki.c1a7862b.jpg"
+  }
+]);
